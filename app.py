5893dd491479c67cc0d7d5f205efba5751e42ca7
Shaik-Nageen <shaiknageen2286@gmail.com>
2025-10-03 15:06:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 ax2 + bx + c = 0, where
 a, b and c are real numbers and
 a ≠ 0
+
+2017 is not a leap year
+1900 is a not leap year
+2012 is a leap year
+2000 is a leap year
+
+  
+2017 is not a leap year
+1900 is a not leap year
+2012 is a leap year
+2000 is a leap year
